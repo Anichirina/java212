5fa38811d84246690424cffeb5c6d36fbbd732a6
Irina Anichkova <anichkova2016@yandex.ru>
2020-11-09 18:07:53 +0300

    new file

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String [] args) {
+       int deposit = 110;
+       int percent;
+        if (deposit>1000) {
+            percent = 1;
+        } else {
+            percent = 0;
+        }
+        int account = 100;
+        int bonus = (int)deposit * (int)percent /100;
+        int result = deposit + account + bonus;
+        System.out.println(result);
+    }
+   }
